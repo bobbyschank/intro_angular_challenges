d6ae67d9a20855ca8c7fecb4e9bccdd98080a892
bobbyschank <robertschank@gmail.com>
2017-04-25 17:02:07 -0600

    challeng 2.1

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,11 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Bob Schank";
+  this.age = '29';
+  this.city = 'Rochester';
+  this.state = 'New York';
+}

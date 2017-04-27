a500af1eef04eba57b2789c2c40e14eadae9df84
bobbyschank <robertschank@gmail.com>
2017-04-26 18:26:34 -0600

    challenge 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,7 +1,8 @@
 console.log('challenge #2 -- app.js loaded!');
 
-var app = angular.module("appTwo", []);
-app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", [])
+	.controller("WelcomeController", WelcomeController)
+	.controller("SecondController", SecondController);
 
 function WelcomeController(){
   this.full_name = "Bob Schank";
@@ -11,4 +12,12 @@ function WelcomeController(){
   this.shout = function(x) {return `${x.toUpperCase()}!`;};
 }
 
-app.controller("")
+function SecondController(){
+	this.class_name = "WDI3";
+	this.enrolled_students = 12;
+	this.start_date = 4;
+	this.end_date = 95;
+	this.daysRemaining = function(e, s) {
+		return e - s;
+	};
+}
